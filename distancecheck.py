18ab3fbbc526402da51b3f17d5af295f7e36aae8
kavyn <motorollllo@gmail.com>
2024-01-25 20:41:11 +0200

    first

--- distancecheck.py
+++ distancecheck.py
@@ -17,6 +17,8 @@ import fLUXreborn
 class ClassName(fLUXreborn.ClassName):
     def custom(self):
         sleep(2)
+        self.arduino.move(-2*self.pi,0)
+        sleep(2)
         self.mousemove(2*self.pi,0)
         sleep(63)
         '''
