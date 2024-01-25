4621502765e6af0f593e1b6c7be4871bd861cf41
kavyn <motorollllo@gmail.com>
2024-01-25 23:37:13 +0200

    first

--- main1.py
+++ main1.py
@@ -117,7 +117,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         #win32gui.SetForegroundWindow(self.hwnd)
         self.region = 1, 2, 3 - 4, 5 - 6
         # initialize the WindowCapture class
-        #self.camera = dxcam.create()
+        self.camera = dxcam.create()
         self.restart = False
         self.selfcast = self.keys['key1']['value']
         self.moveback = self.keys['key2']['value']
@@ -192,8 +192,8 @@ class ClassName(BaseScript):  # Название класса (должен от
         self.ban = False
         self.mousemovetimer = time()
         self.camerastop = False
-        #self.c = Thread(target=self.videocamera, args=())
-        #self.c.start()
+        self.c = Thread(target=self.videocamera, args=())
+        self.c.start()
         self.reconnection = False
         sleep(1)
         self.inittimer = time()
