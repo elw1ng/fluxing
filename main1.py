3d9cf1db8f30a23658b566f53771a9412ab133f3
kavyn <motorollllo@gmail.com>
2024-01-25 22:55:27 +0200

    first

--- main1.py
+++ main1.py
@@ -4,7 +4,8 @@ print(1231/1)
 arduino = Arduino(port = 'COM3')
 
 while True:
+    print("asdw")
     for i in range(1111):
-        arduino.move(1,0)
+        arduino.move(55,0)
     for i in range(1111):
-        arduino.move(-1,0)
+        arduino.move(-55,0)
