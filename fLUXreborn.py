8cd002210e965e82de86d54ab73067c1c9e3bfcd
kavyn <motorollllo@gmail.com>
2024-01-26 18:44:58 +0200

    first

--- fLUXreborn.py
+++ fLUXreborn.py
@@ -687,17 +687,15 @@ class ClassName(BaseScript):  # Название класса (должен от
             # sleep(1 / 60)
             self.getNextFrame()
             Prediction = self.model.predict(source= self.img[160:480][160:480], device=0, conf=conf, iou=0.3,imgsz=320)
-            detected_boxes = Prediction[0].boxes
+            detected_boxes = np.array(Prediction[0].boxes.cpu())
             if len(detected_boxes) >= 1:
                 for box in detected_boxes:
                     if (box.cls == checkbox.cls):
-                        xyxy = np.array(box.xyxy.cpu())
-                        xyxy[0][0] += 160
-                        xyxy[0][1] += 160
-                        xyxy[0][2] += 160
-                        xyxy[0][3] += 160
-                        t = torch.Tensor(xyxy)
-                        box.xyxy = t.to(torch.device("cuda:0"))
+
+                        box.xyxy[0][0] += 160
+                        box.xyxy[0][1] += 160
+                        box.xyxy[0][2] += 160
+                        box.xyxy[0][3] += 160
                         sizeDiff = abs(box.xyxy[0][2] - box.xyxy[0][0] - checkbox.xyxy[0][2] + checkbox.xyxy[0][0])
                         dist = self.checkDistance(box)
                         if sizeDiff < 6 and dist < 6*(j+1):
