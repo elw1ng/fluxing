fbaec6cdb689526817174ec872e2681cac7bd171
kavyn <motorollllo@gmail.com>
2024-01-26 13:42:31 +0200

    first

--- fLUXreborn.py
+++ fLUXreborn.py
@@ -681,7 +681,7 @@ class ClassName(BaseScript):  # Название класса (должен от
             found = False
             # sleep(1 / 60)
             self.getNextFrame()
-            Prediction = self.model.predict(source=self.img, device=0, conf=conf, iou=0.3)
+            Prediction = self.model.predict(source=self.img[220:420][220:420], device=0, conf=conf, iou=0.3)
             detected_boxes = Prediction[0].boxes
             if len(detected_boxes) >= 1:
                 for box in detected_boxes:
