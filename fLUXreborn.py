3348c3ebddd6926804e9aa8c392603bb6d07c1a5
kavyn <motorollllo@gmail.com>
2024-01-27 17:37:54 +0200

    first

--- fLUXreborn.py
+++ fLUXreborn.py
@@ -139,7 +139,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         self.USER1_ID = self.keys['key17']['value']
         self.USER2_ID = self.keys['key18']['value']
         self.TOKEN = self.keys['key19']['value']
-        self.target_fps = 56
+        self.target_fps = 50
         self.holdtime = time()
         if "fps" in sys.argv:
             self.target_fps = int(sys.argv[sys.argv.index("fps") + 1])
