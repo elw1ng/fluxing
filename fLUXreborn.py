95e4482eaefd9cae5f52c345905d970ec2b3bd8e
kavyn <motorollllo@gmail.com>
2024-01-27 21:19:59 +0200

    first

--- fLUXreborn.py
+++ fLUXreborn.py
@@ -139,7 +139,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         self.USER1_ID = self.keys['key17']['value']
         self.USER2_ID = self.keys['key18']['value']
         self.TOKEN = self.keys['key19']['value']
-        self.target_fps = 56
+        self.target_fps = 60
         self.holdtime = time()
         if "fps" in sys.argv:
             self.target_fps = int(sys.argv[sys.argv.index("fps") + 1])
@@ -206,7 +206,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         self.y = 0.01
         self.yelipse = 1.0
         self.overload = 0
-        self.aftermovedelay = 0.051
+        self.aftermovedelay = 0.0465
         if "R" in sys.argv:
             self.R = float(sys.argv[sys.argv.index("R") + 1])
         if "e" in sys.argv:
@@ -454,21 +454,23 @@ class ClassName(BaseScript):  # Название класса (должен от
             self.img = img
 
     def getNextFrame(self, throttle=0.0):
-        self.camerastop = True
+
         if self.t is not None:
+            self.camerastop = True
             self.t.join()
             self.t = None
 
-            sleep(self.aftermovedelay - 1 / self.target_fps)
-
+            sleep(self.aftermovedelay)
+            img = self.camera.get_latest_frame()
+            self.fpstimer = time()
+            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+            self.img = img
+            self.camerastop = False
+        else:
             # print("asdasd")
         # print("asdasd1")
-        sleep(1 / self.target_fps)
-        img = self.camera.get_latest_frame()
-        self.fpstimer = time()
-        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-        self.img = img
-        self.camerastop = False
+            sleep(1 / self.target_fps+0.004)
+
         '''
         while time() - self.fpstimer < (1 / self.target_fps):
             sleep(0.001)
