70d194ce404033c0e6089ef89d2da20e1b66572d
kavyn <motorollllo@gmail.com>
2024-01-28 20:29:30 +0200

    first

--- fLUXreborn.py
+++ fLUXreborn.py
@@ -270,7 +270,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         type = random.randint(1,4)
         if type <4:
 
-            beta = random.randint(int(-self.pi/ 3.3 / type), int(self.pi/ 3.3 / type))
+            beta = random.randint(int(-self.pi/ 4.1 / type), int(self.pi/ 4.1 / type))
             rads = self.to_rad(beta)
             alp = self.fromcenteralp()
             d = 13.4
