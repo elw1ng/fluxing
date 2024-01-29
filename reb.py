0a72a3df40b027f431bbf56844950a1d5aa67bbd
kavyn <motorollllo@gmail.com>
2024-01-29 21:52:54 +0200

    first

--- reb.py
+++ reb.py
@@ -544,7 +544,7 @@ class ClassName(BaseScript):  # Название класса (должен от
         sleep(0.1)
         self.lkmrelease()
         sleep(0.1)
-    def mousemove(self, x, y, limiter=14):
+    def mousemove(self, x, y, limiter=16):
         self.arduino.move(x, y, limiter)
         #self.aftermovedelay= 0.05+0.015*(abs(x)+abs(y))/120
         #if self.aftermovedelay > 0.08:
